df1c029247a648dc0353c92b5ca815d1d814083d
ek-170 <klose.foot.baller@gmail.com>
2024-12-22 14:55:32 +0900

    add hack

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+@i
+M=0
+@R2
+M=0
+(LOOP)
+@i
+M=M+1
+D=M
+@R1
+D=D-M // i - R1
+@END
+D;JGT
+
+@R0
+D=M
+@R2
+M=M+D
+@LOOP
+0;JMP
+(END)
+
+@END
+0;JMP
